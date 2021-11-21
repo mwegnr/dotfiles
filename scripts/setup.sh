99c22744c87d912233f5e4816decd8926c0290da
Maik <maikwegener@mailbox.org>
2021-11-21 20:20:36 +0100

    move configs to folder, add install script, change to unencrypted home (only for installation)

---
+++ scripts/setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# install git and pip
+pacman -Syy git python-pip
+
+# download dotfiles & arch installer
+git clone https://github.com/angerstoner/dotfiles
+git clone https://github.com/archlinux/archinstall
+
+# replace installed archinstall by newer version
+pip uninstall archinstall -y
+cd archinstall
+git checkout v2.3.0.RC2
+python setup.py install
+
+# launch archinstall with config
+cd examples
+cp ~/dotfiles/arch_install/* . 
+
+python guided.py --config /root/dotfiles/arch_install/config.json --silent --disk_layouts=/root/dotfiles/arch_install/disk_layout
