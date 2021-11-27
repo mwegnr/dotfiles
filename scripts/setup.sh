a4cdab1b68ff0849d5a3a1e2e00abb65b97d2c4b
Maik <maikwegener@mailbox.org>
2021-11-27 21:17:57 +0100

    fixed profile path, switched to archinstaller in repos

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,26 +1,15 @@
 #!/usr/bin/env bash
 
-PASSWORD="test"
-
-# install git and pip
-pacman -Sy --noconfirm git python-pip
+# install git and update archinstall to at least v2.3.0
+pacman -Sy --noconfirm git archinstall
 
 # download dotfiles & arch installer
 git clone https://github.com/angerstoner/dotfiles
 git clone https://github.com/archlinux/archinstall
 
-# replace installed archinstall by newer version
-pip uninstall archinstall -y
-cd archinstall
-git checkout v2.3.0
-python setup.py install
-
 # launch archinstall with config
-cd examples
-cp ~/dotfiles/arch_install/* .
-
-python guided.py --config /root/dotfiles/arch_install/config.json --silent --disk_layouts=/root/dotfiles/arch_install/disk_layout.json
+archinstall --config /root/dotfiles/arch_install/config.json --silent --disk_layouts=/root/dotfiles/arch_install/disk_layout.json
 
 arch-chroot /mnt/archinstall
 useradd -m angerstoner
-echo -e "$PASSWORD\n$PASSWORD"  | passwd angerstoner
+echo -e "devel\ndevel"  | passwd angerstoner
