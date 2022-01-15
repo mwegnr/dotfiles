cead1577c1bf85b7fe48933224e668ca1e5ea96a
Maik <maikwegener@mailbox.org>
2022-01-15 18:14:46 +0100

    fix configs path in install.py

--- scripts/install.py
+++ scripts/install.py
@@ -3,18 +3,19 @@ import subprocess
 import os.path
 
 USER_HOME = "/home/angerstoner"
+CONFIGS_PATH = "configs"
 
 config_paths = {
-    "gtk2.0.config": f"{USER_HOME}/.gtkrc-2.0",
-    "gtk3.0.config": f"{USER_HOME}/.config/gtk-3.0/settings.ini",
-    "import-gsettings": f"{USER_HOME}/.config/gtk-3.0/import-gsettings",
-    "sway.config": f"{USER_HOME}/.config/sway/config",
-    "termite.config": f"{USER_HOME}/.config/termite/config",
-    "waybar.config": f"{USER_HOME}/.config/waybar/config",
-    "waybar.style": f"{USER_HOME}/.config/waybar/style.css",
-    "zsh.config": f"{USER_HOME}/.zshrc",
-    "nvim.config": f"{USER_HOME}/.config/nvim/init.vim",
-    "latexmk.config": f"{USER_HOME}/.latexmkrc"
+    f"{CONFIGS_PATH}/gtk2.0.config": f"{USER_HOME}/.gtkrc-2.0",
+    f"{CONFIGS_PATH}/gtk3.0.config": f"{USER_HOME}/.config/gtk-3.0/settings.ini",
+    f"{CONFIGS_PATH}/import-gsettings": f"{USER_HOME}/.config/gtk-3.0/import-gsettings",
+    f"{CONFIGS_PATH}/sway.config": f"{USER_HOME}/.config/sway/config",
+    f"{CONFIGS_PATH}/termite.config": f"{USER_HOME}/.config/termite/config",
+    f"{CONFIGS_PATH}/waybar.config": f"{USER_HOME}/.config/waybar/config",
+    f"{CONFIGS_PATH}/waybar.style": f"{USER_HOME}/.config/waybar/style.css",
+    f"{CONFIGS_PATH}/zsh.config": f"{USER_HOME}/.zshrc",
+    f"{CONFIGS_PATH}/nvim.config": f"{USER_HOME}/.config/nvim/init.vim",
+    f"{CONFIGS_PATH}/latexmk.config": f"{USER_HOME}/.latexmkrc"
 }
 
 
