5e9732ecb4cf5d33ed27f8bf06287a03e96c6602
Maik <maikwegener@mailbox.org>
2022-01-15 16:26:14 +0100

    add dotfile folder creation to link script

--- scripts/install.py
+++ scripts/install.py
@@ -2,17 +2,19 @@
 import subprocess
 import os.path
 
+USER_HOME = "/home/angerstoner"
+
 config_paths = {
-    "gtk2.0.config": "/home/angerstoner/.gtkrc-2.0",
-    "gtk3.0.config": "/home/angerstoner/.config/gtk-3.0/settings.ini",
-    "import-gsettings": "/home/angerstoner/.config/gtk-3.0/import-gsettings",
-    "sway.config": "/home/angerstoner/.config/sway/config",
-    "termite.config": "/home/angerstoner/.config/termite/config",
-    "waybar.config": "/home/angerstoner/.config/waybar/config",
-    "waybar.style": "/home/angerstoner/.config/waybar/style.css",
-    "zsh.config": "/home/angerstoner/.zshrc",
-    "nvim.config": "/home/angerstoner/.config/nvim/init.vim",
-    "latexmk.config": "/home/angerstoner/.latexmkrc"
+    "gtk2.0.config": f"{USER_HOME}/.gtkrc-2.0",
+    "gtk3.0.config": f"{USER_HOME}/.config/gtk-3.0/settings.ini",
+    "import-gsettings": f"{USER_HOME}/.config/gtk-3.0/import-gsettings",
+    "sway.config": f"{USER_HOME}/.config/sway/config",
+    "termite.config": f"{USER_HOME}/.config/termite/config",
+    "waybar.config": f"{USER_HOME}/.config/waybar/config",
+    "waybar.style": f"{USER_HOME}/.config/waybar/style.css",
+    "zsh.config": f"{USER_HOME}/.zshrc",
+    "nvim.config": f"{USER_HOME}/.config/nvim/init.vim",
+    "latexmk.config": f"{USER_HOME}/.latexmkrc"
 }
 
 
@@ -26,6 +28,7 @@ def link_config_files():
             os.path.split(os.path.dirname(__file__))[0],  # parent dir/config file dir +
             file)  # filename
 
+        os.makedirs(filepath, exist_ok=True)
         link_cmd = ["ln", "-sf", filepath, config_paths[file]]
         subprocess.run(link_cmd)
         print(link_cmd)
